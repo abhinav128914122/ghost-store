aa2ed86118a4d18dd48f9447aeac08b2b0ffc7cf
Abhinav Kumar <abhi9235nav@gmail.com>
2026-03-22 14:57:16 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,13 @@
-
+const S = {
+  user: null,
+  profile: null,
+  products: [],
+  cat: 'All',
+  search: '',
+  curProduct: null,
+  pay: { product: null, discount: 0, couponCode: '' },
+  reviewRating: 0,
+  selectedTopup: 0,
+  pendingAction: null,
+  wishlist: []
+};
